b8d543416aea00ac8599967937a3d32741db418e
BulatovaTati <kohodatat@gmail.com>
2025-01-06 20:15:07 +0200

    add new project

--- src/js/readmore.js
+++ src/js/readmore.js
@@ -1,8 +1,100 @@
+import movies from '../img/projects/movies.png';
+import yachts from '../img/projects/jet.jpg';
+import chocolate from '../img/projects/chocolate.png';
+import harvest from '../img/projects/veges.jpg';
+import food from '../img/projects/food.png';
+import barber from '../img/projects/barber.jpg';
+import ice from '../img/projects/ice.jpg';
+import donuts from '../img/projects/donuts.jpg';
+import landing from '../img/projects/landing.jpg';
+
+const projects = [
+  {
+    title: 'Landing Page',
+    image: landing,
+    alt: 'Landing Page',
+    description:
+      'This website is Lloyd`s  personal portfolio where He can not only demonstrate his skillset, but also talk about his experiences and past projects that have brought him to where He is now.',
+    codeLink: 'https://github.com/BulatovaTati/landing-page-2.0',
+    projectLink: 'https://bulatovatati.github.io/landing-page-2.0/',
+  },
+  {
+    title: 'Movies Source',
+    image: movies,
+    alt: 'Movies Source',
+    description:
+      'A movie information platform that pulls data from a public API for movie details.',
+    codeLink: 'https://github.com/BulatovaTati/movies-source',
+    projectLink: 'https://bulatovatati.github.io/movies-source/',
+  },
+  {
+    title: 'Yacht Jet',
+    image: yachts,
+    alt: 'Yacht Jet',
+    description:
+      'A luxury yacht rental platform showcasing various yachts available for rent.',
+    codeLink: 'https://github.com/BulatovaTati/Yacht-Jet',
+    projectLink: 'https://bulatovatati.github.io/Yacht-Jet/',
+  },
+  {
+    title: 'SIMPLY CHOCOLATE',
+    image: chocolate,
+    alt: 'SIMPLY-CHOCOLATE',
+    description:
+      'A responsive website for a chocolate brand, featuring smooth scroll for navigation.',
+    codeLink: 'https://github.com/BulatovaTati/Simply-Chocolate',
+    projectLink: 'https://bulatovatati.github.io/SIMPLY-CHOCOLATE/',
+  },
+  {
+    title: 'Green Harvest',
+    image: harvest,
+    alt: 'Green Harvest',
+    description:
+      'A project focused on promoting healthy eating with engaging content and resources.',
+    codeLink: 'https://github.com/BulatovaTati/GreenHarvest',
+    projectLink: 'https://bulatovatati.github.io/GreenHarvest/',
+  },
+  {
+    title: 'Food Delivery',
+    image: food,
+    alt: 'Food Delivery',
+    description:
+      'A food delivery app that allows users to browse menus, place orders, and track deliveries.',
+    codeLink: 'https://github.com/BulatovaTati/food-delivery',
+    projectLink: 'https://bulatovatati.github.io/food-delivery/',
+  },
+  {
+    title: 'Barber Shop',
+    image: barber,
+    alt: 'Barber Shop',
+    description:
+      'A stylish and functional website for a barbershop, featuring a booking system.',
+    codeLink: 'https://github.com/BulatovaTati/BarberShop',
+    projectLink: 'https://bulatovatati.github.io/BarberShop/',
+  },
+  {
+    title: 'Ice Cream',
+    image: ice,
+    alt: 'Ice Cream',
+    description:
+      'A visually appealing website showcasing a range of ice cream products.',
+    codeLink: 'https://github.com/BulatovaTati/IceCream',
+    projectLink: 'https://bulatovatati.github.io/IceCream/',
+  },
+  {
+    title: 'Donats',
+    image: donuts,
+    alt: 'Donats',
+    description: 'A fun and interactive website for ordering donuts online.',
+    codeLink: 'https://github.com/BulatovaTati/Donats',
+    projectLink: 'https://bulatovatati.github.io/Donats/',
+  },
+];
+
 let PROJECTS_PER_PAGE;
 let currentPage = 1;
 const loadMoreBtn = document.querySelector('.load-more');
 const projectsList = document.querySelector('.projects-list');
-const allProjects = Array.from(projectsList.children);
 
 function setProjectsPerPage() {
   if (window.innerWidth >= 1280) {
@@ -15,32 +107,38 @@ function setProjectsPerPage() {
 
   currentPage = 1;
 
-  allProjects.forEach((project, index) => {
+  const projectItems = document.querySelectorAll('.projects-item');
+
+  projectItems.forEach((project, index) => {
     project.style.display = index < PROJECTS_PER_PAGE ? 'block' : 'none';
   });
 
   loadMoreBtn.style.display =
-    PROJECTS_PER_PAGE < allProjects.length ? 'block' : 'none';
+    PROJECTS_PER_PAGE < projectItems.length ? 'block' : 'none';
 }
 
 function onLoadMore() {
+  const projectItems = document.querySelectorAll('.projects-item');
+
   const startIndex = currentPage * PROJECTS_PER_PAGE;
   const endIndex = startIndex + PROJECTS_PER_PAGE;
 
-  for (let i = startIndex; i < endIndex && i < allProjects.length; i++) {
-    allProjects[i].style.display = 'block';
+  for (let i = startIndex; i < endIndex && i < projectItems.length; i++) {
+    projectItems[i].style.display = 'block';
   }
 
   currentPage++;
   smoothScrolling();
 
-  if (endIndex >= allProjects.length) {
+  if (endIndex >= projectItems.length) {
     loadMoreBtn.style.display = 'none';
   }
 }
 
 function smoothScrolling() {
-  const { height: cardHeight } = allProjects[0].getBoundingClientRect();
+  const { height: cardHeight } = document
+    .querySelector('.projects-list')
+    .firstElementChild.getBoundingClientRect();
 
   window.scrollBy({
     top: cardHeight,
@@ -50,5 +148,45 @@ function smoothScrolling() {
 
 loadMoreBtn.addEventListener('click', onLoadMore);
 window.addEventListener('resize', setProjectsPerPage);
+projectsList.insertAdjacentHTML(
+  'beforeend',
+  productMarkup(projects, 0, PROJECTS_PER_PAGE)
+);
 
 setProjectsPerPage();
+
+function productMarkup(projects, startIndex, endIndex) {
+  return projects
+    .slice(startIndex, endIndex)
+    .reduce(
+      (acc, { title, image, alt, description, codeLink, projectLink }) => {
+        return (
+          acc +
+          ` <li class="projects-item">
+        <img
+          class="projects-img"
+          src="${image}"
+          alt="${alt}"
+        />
+        <h3 class="projects-item-title">${title}</h3>
+        <p class="projects-item-text">
+         ${description}
+        </p>
+        <a
+          href="${codeLink}"
+          class="projects-code-link"
+          target="_blank"
+          >View Code</a
+        >
+        <a
+         href="${projectLink}"
+          class="projects-project-link"
+          target="_blank"
+          >View Project</a
+        >
+      </li>`
+        );
+      },
+      ''
+    );
+}
